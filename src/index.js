fb2c789c06142977f3bff0ba9ad51d0b5fc92b25
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-11-30 14:05:49 -0700

    started adding style changes for events

--- src/index.js
+++ src/index.js
@@ -3,8 +3,48 @@ import './less/index.less'
 // Your code goes here!
 const links = document.querySelectorAll('nav a');
 const navLinks = Array.from(links);
+//4 links
+function greenText() {
+    event.target.style.color = 'green';
+};
+navLinks[0].addEventListener('dblclick', greenText);
+navLinks[1].addEventListener('wheel', greenText);
+navLinks[2].addEventListener('copy', greenText);
+navLinks[3].addEventListener('paste', greenText);
 
 const busClicker = document.querySelector('.intro img');
+//bus image
+function redBus() {
+    document.body.style.backgroundColor = 'red';
+};
+busClicker.addEventListener('mouseover', redBus);
 
-const buttons = document.querySelectorAll('.content-pick .btn');
-const signButtons = Array.from(buttons);
+
+
+const sign = document.querySelectorAll('.btn');
+const signButtons = Array.from(sign);
+//3 buttons
+function purpleText() {
+    event.target.style.color = 'purple';
+};
+function purpleBox() {
+    event.target.style.backgroundColor = 'purple';
+};
+signButtons[0].addEventListener('cut', purpleText);
+signButtons[1].addEventListener('', purpleBox);
+signButtons[2].addEventListener('', purpleText);
+
+
+const copyright = document.querySelector('.footer p');
+//copyright section
+function blueText() {
+    document.body.style.color = 'blue';
+};
+copyright.addEventListener('click', blueText);
+
+
+
+const funBus = document.querySelector('.logo-heading');
+//fun bus heading
+
+
