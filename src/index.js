0e16f609f318feef5ab203b0c0056eade8d57e82
kylerostler <93017344+kylerostler@users.noreply.github.com>
2021-11-30 13:28:06 -0700

    started adding querySelectors

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './less/index.less'
 
 // Your code goes here!
+const links = document.querySelectorAll('nav a');
+const navLinks = Array.from(links);
+
+const busClicker = document.querySelector('.intro img');
+
+const buttons = document.querySelectorAll('.content-pick .btn');
+const signButtons = Array.from(buttons);
